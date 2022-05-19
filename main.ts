a3d7720242faff20ef68d5829aa20330519712a6
JohnJVT <104933633+JohnJVT@users.noreply.github.com>
2022-05-19 16:47:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,162 @@
+namespace SpriteKind {
+    export const Bullet = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 5 5 5 . . . . . 
+        . . . . 5 4 4 2 4 4 5 . . . . . 
+        . . . . 5 4 2 2 2 4 5 . . . . . 
+        . . . . 5 4 2 2 2 4 5 . . . . . 
+        . . . . 5 5 4 4 4 5 5 . . . . . 
+        . . . . 5 5 5 5 5 5 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Ship, 0, -100)
+})
+let projectile: Sprite = null
+let Ship: Sprite = null
+let Astroids = [
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c . . . . . . 
+    . . . . . . a b a a . . . . . . 
+    . . . . . c b a f c a c . . . . 
+    . . . . c b b b f f a c c . . . 
+    . . . . b b f a b b a a c . . . 
+    . . . . c b f f b a f c a . . . 
+    . . . . . c a a c b b a . . . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . . c . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . . . . c c 8 . . . . 
+    . . . . . . 8 c c c f 8 c c . . 
+    . . . c c 8 8 f c a f f f c c . 
+    . . c c c f f f c a a f f c c c 
+    8 c c c f f f f c c a a c 8 c c 
+    c c c b f f f 8 a c c a a a c c 
+    c a a b b 8 a b c c c c c c c c 
+    a f c a a b b a c c c c c f f c 
+    a 8 f c a a c c a c a c f f f c 
+    c a 8 a a c c c c a a f f f 8 a 
+    . a c a a c f f a a b 8 f f c a 
+    . . c c b a f f f a b b c c 6 c 
+    . . . c b b a f f 6 6 a b 6 c . 
+    . . . c c b b b 6 6 a c c c c . 
+    . . . . c c a b b c c c . . . . 
+    . . . . . c c c c c c . . . . . 
+    `,
+img`
+    . . . . . . . . c c c c . . . . 
+    . . . . c c c c c c c c c . . . 
+    . . . c f c c a a a a c a c . . 
+    . . c c f f f f a a a c a a c . 
+    . . c c a f f c a a f f f a a c 
+    . . c c a a a a b c f f f a a c 
+    . c c c c a c c b a f c a a c c 
+    c a f f c c c a b b 6 b b b c c 
+    c a f f f f c c c 6 b b b a a c 
+    c a a c f f c a 6 6 b b b a a c 
+    c c b a a a a b 6 b b a b b a . 
+    . c c b b b b b b b a c c b a . 
+    . . c c c b c c c b a a b c . . 
+    . . . . c b a c c b b b c . . . 
+    . . . . c b b a a 6 b c . . . . 
+    . . . . . . b 6 6 c c . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `,
+img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . a c a a . . . . . . . 
+    . . . a a c c c a a . . . . . . 
+    . . a a c c c c c a a . . . . . 
+    . . a c c c c c c c a . . . . . 
+    . . a c c c c c c c a a . . . . 
+    . . a c c c c c c c c a . . . . 
+    . . a c c c c c c c c a . . . . 
+    . . a a c c c c c c c a . . . . 
+    . . . a c c c c c c a a . . . . 
+    . . . a a c c c c a a . . . . . 
+    . . . . a a a a a . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . . . . c c c . . . . . . . 
+    . . . . . a a a c c c . . . . . 
+    . . . c a c f a a a a c . . . . 
+    . . c a c f f f a f f a c . . . 
+    . c c a c c f a a c f f a c . . 
+    . a b a a c 6 a a c c f a c c c 
+    . a b b b 6 a b b a a c a f f c 
+    . . a b b a f f b b a a c f f c 
+    c . a a a c c f c b a a c f a c 
+    c c a a a c c a a a b b a c a c 
+    a c a b b a a 6 a b b 6 b b c . 
+    b a c b b b 6 b c . c c a c . . 
+    b a c c a b b a c . . . . . . . 
+    b b a c a b a a . . . . . . . . 
+    a b 6 b b a c . . . . . . . . . 
+    . a a b c . . . . . . . . . . . 
+    `
+]
+Ship = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 2 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . e 2 . . . . . . . 
+    . . . . . . e e 4 e . . . . . . 
+    . . . . . . e 2 4 e . . . . . . 
+    . . . . . c c c e e e . . . . . 
+    . . . . e e 2 2 2 4 e e . . . . 
+    . . c f f f c c e e f f e e . . 
+    . c c c c e e 2 2 2 2 4 2 e e . 
+    c c c c c c e e 2 2 2 4 2 2 e e 
+    c c c c c c e e 2 2 2 2 4 2 e e 
+    `, SpriteKind.Player)
+Ship.setBounceOnWall(true)
+controller.moveSprite(Ship, 100, 100)
+info.setLife(3)
+effects.starField.startScreenEffect()
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(Astroids[randint(0, 1 - Astroids.length)], 0, 74)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
+})
